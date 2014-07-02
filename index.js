ff65533acd97c7f3cc6c2c6f8787e9c5e2dadd06
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-02 19:47:51 +0000

    getting pages loaded

--- index.js
+++ index.js
@@ -32,11 +32,18 @@ function PageTurner(elem, pageSelector, options){
   this.elem = elem
   this.pageSelector = pageSelector
 
-  this.pages = this.elem.querySelectorAll(this.pageSelector);
+  this.pages = []
+  var pageResults = this.elem.querySelectorAll(this.pageSelector)
+
+  for(var i=0; i<pageResults.length; i++){
+    this.pages.push(pageResults[i])
+  }
 
   if(this.pages.length<=0){
     throw new Error('pageturner cannot find any pages for the book');
   }
+
+  console.dir(this.pages)
 
   options = this.options = options || {};
 
@@ -48,20 +55,20 @@ function PageTurner(elem, pageSelector, options){
 
   this.is3d = tools.is3d()
   
-  this.page_html = [];
-  this.pages = [];
-  this.currentpage = 0;
+  this.page_html = []
+  this.currentpage = 0
 }
 
-Emitter(PageTurner.prototype)
+//Emitter(PageTurner.prototype)
 
 PageTurner.prototype.render = function(target){
   var self = this;
 
-  target = target || this.elem
-  
-  this.pages.forEach(function(page){
+  target = target || self.elem
+
+  self.pages.forEach(function(page){
     console.log('-------------------------------------------');
     console.dir(page)
   })
+
 }
